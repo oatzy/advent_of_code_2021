cd216091787d76c8b523054e4594615579d48c68
Chris Oates <kryz.oatz@gmail.com>
2021-12-01 22:03:38 +0000

    day01, both parts in a single awk

--- day01/day01.sh
+++ day01/day01.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-awk 'NR>1 {inc += ($0 > prev)} {prev = $0} END {print inc}' input.txt
-
 # this is dumb :p
-awk 'NR>3 {inc += ($0 > prev3)} NR>2 {prev3 = prev2} NR>1 {prev2 = prev} {prev = $0} END {print inc}' input.txt
+awk 'NR>3 {inc2 += ($0 > prev3)} NR>2 {prev3 = prev2} NR>1 {inc1 += ($0 > prev); prev2 = prev} {prev = $0} END {print inc1 "\n" inc2}' "${1:-input.txt}"
